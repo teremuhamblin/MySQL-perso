d2307705f75888a4af697483046bbc20f8170efa
98731 <105843577+teremuhamblin@users.noreply.github.com>
2026-09-21 12:16:17 +0200

    Add initial notes to the database

---
+++ insert.sql
@@ -0,0 +1,7 @@
+USE my_personal_db;
+
+INSERT INTO notes (title, content)
+VALUES 
+('Première note', 'Ceci est ma première note personnelle.'),
+('Courses', 'Acheter du lait, du pain, des œufs.'),
+('Idée', 'Créer un mini projet MySQL perso v1.0.');
